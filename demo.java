3cbaccae1412d408617a4d2f0e790203f9ec1e3d
Budhi <gurung.b1t@gmail.com>
2022-06-08 13:24:57 -0400

    demo class with multiply function

---
+++ demo.java
@@ -0,0 +1,11 @@
+package test;
+
+public class demo{
+    public static void main(String[] args) {
+      System.out.println( multiply(25, 25));
+    }
+
+    public static int multiply(int num1, int num2){
+        return num1*num2;
+    }
+}
