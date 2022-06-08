c2cd71c4b816afcd7cdeb3f74acbfbf76d731570
Budhi <gurung.b1t@gmail.com>
2022-06-08 13:20:47 -0400

    added printName function

--- helloWorld.java
+++ helloWorld.java
@@ -3,5 +3,10 @@ package test;
 public class helloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        printName();
+    }
+
+    public static void printName(){
+        System.out.println("My name is Budhi Gurung");
     }
 }
